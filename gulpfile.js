9e371cb42962b2b1b050b41a56c6de70980ec9f6
aipacommander <gundamgundam777@gmail.com>
2015-11-19 14:24:01 +0900

    [add] complie scss and mifiyed css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp       = require('gulp');
+var sass       = require('gulp-sass');
+var minifyCss  = require('gulp-minify-css');
+var src        = '.';
 
 gulp.task('sass', function() {
-  gulp.src('sass/**/*scss')
+  gulp.src(src + '/sass/*scss')
     .pipe(sass())
-    .pipe(gulp.dest('./css'));
+    .pipe(minifyCss())
+    .pipe(gulp.dest(src + '/css'));
+});
+
+// watch
+gulp.task('watch', function() {
+  var w_sass = gulp.watch(src + '/sass/*scss', ['sass']);
+  w_sass.on('change', function(event){
+    console.log('CSS File ' + event.path + ' was ' + event.type + ', running task sass...');
+  });
 });
